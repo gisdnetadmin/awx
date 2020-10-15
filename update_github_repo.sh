426621be6657b340b7b02c33a41fb68bc516b04f
gisdnetadmin <netadmin@granburyisd.org>
2020-10-15 16:22:02 -0500

    update commit

--- update_github_repo.sh
+++ update_github_repo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 /usr/bin/git add /var/lib/awx/projects/*
-/usr/bin/git remote add origin git@github.com:gisdnetadmin/ansible_awx.git
+/usr/bin/git remote add origin git@github.com:gisdnetadmin/awx.git
 /usr/bin/git commit -m "update commit"
 /usr/bin/git branch -M main
 /usr/bin/git push -u origin main
